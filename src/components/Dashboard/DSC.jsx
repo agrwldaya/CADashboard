7e8da2738187a77d6fbb662fb074dbe0e96963b4
agrwldaya <agrawaldaya336@gmail.com>
2025-03-28 00:37:20 +0530

    export

--- src/components/Dashboard/DSC.jsx
+++ src/components/Dashboard/DSC.jsx
@@ -445,7 +445,7 @@ const DscManager = () => {
 
               <button
                 onClick={() => handleExport("expiring")}
-                className="w-full bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition-colors duration-200"
+                className="w-full bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition-colors duration-200"
               >
                 Export Expiring
               </button>
